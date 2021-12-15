65039e61cc8b044ac1effff8ffdca4f5af7a3398
jiagengchang <jiageng1997@gmail.com>
2021-12-15 19:59:42 +0000

    Changed include.h

--- include/mlp.h
+++ include/mlp.h
@@ -10,34 +10,36 @@
 #define NUM_HIDDEN 3 //number of hidden units
 #define NUM_OUTPUT 8 //number of output units
 #define NUM_PARAMS NUM_INPUT*NUM_HIDDEN+NUM_INPUT*NUM_OUTPUT //number of trainable parameters
-#define learningRate 1e-4
-
 #define NUM_TRAIN 8 //size of training data, no test data here
 
 typedef struct {
-	float input;
-	float activation;
-	float delta;
-	float bias;
+	double input;
+	double activation;
+	double derivative;
+	double delta;
 } Unit;
 
 typedef struct {
 	Unit input[NUM_INPUT]; 
 	Unit hidden[NUM_HIDDEN];
 	Unit output[NUM_OUTPUT];
-	float weights[NUM_PARAMS]; //best to be a 1d array as connections may not be symmetric
+	double weights[NUM_PARAMS]; //best to be a 1d array as connections may not be symmetric
+	double biases[NUM_HIDDEN + NUM_OUTPUT]
 } MultiLayerPerceptron;
 
 
 void resetUnit(Unit *unit);
 void resetNetwork(MultiLayerPerceptron *mlp);
 void activate(Unit *unit);
-void initializeRandomWeights(MultiLayerPerceptron *mlp, float plus_minus_half_of);
-void forwardPass(MultiLayerPerceptron *mlp, float data[NUM_INPUT]);
-void backwardPass(MultiLayerPerceptron *mlp, float signal[NUM_OUTPUT]);
-void train(MultiLayerPerceptron *mlp, const float input[NUM_TRAIN][NUM_INPUT], int numIterations);
-float scoreNetwork(MultiLayerPerceptron *mlp, const float input[NUM_TRAIN][NUM_INPUT]);
-void parseInput(char *fn, float input[NUM_TRAIN][NUM_INPUT]);
-void echoInput(float input[NUM_TRAIN][NUM_INPUT]);
+void initialize(MultiLayerPerceptron *mlp, double plus_minus_half_of);
+void forwardPass(MultiLayerPerceptron *mlp, const double data[NUM_INPUT]);
+void backwardPass(MultiLayerPerceptron *mlp, const double signal[NUM_OUTPUT], const double learningRate);
+void train(MultiLayerPerceptron *mlp, const double input[NUM_TRAIN][NUM_INPUT], int numIterations, const double learningRate);
+void test(MultiLayerPerceptron *mlp, const double input[NUM_TRAIN][NUM_INPUT]);
+void error(MultiLayerPerceptron *mlp, const double input[NUM_TRAIN][NUM_INPUT], double *error);
+void evaluate(MultiLayerPerceptron *mlp, const double input[NUM_TRAIN][NUM_INPUT], unsigned int *ncorrect);
+void parseInput(char *fn, double input[NUM_TRAIN][NUM_INPUT]);
+void echoInput(double input[NUM_TRAIN][NUM_INPUT]);
+void printParams(MultiLayerPerceptron *mlp);
 
 #endif
