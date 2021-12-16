ecfce2fb45b26bee484bad626224c103ec6aa9e3
JiaGengChang <jiageng1997@gmail.com>
2021-12-16 16:02:20 +0000

    Mlp stuff

--- src/testmlp.c
+++ src/testmlp.c
@@ -1,11 +1,14 @@
 #include <utils.h>
 
-void train(int numIterations)
+double sigmoid(double x)
 {
-	int nIter;
-
+	return (1/(1 + exp(-x)));
 }
 
+double dsigmoid(double x) //derivative of sigmoid
+{
+	return (sigmoid(x)*(1-sigmoid(x)));
+}
 
 int main()
 {
@@ -19,16 +22,98 @@ int main()
 		0,0,0,0,0,0,1,0,
 		0,0,0,0,0,0,0,1
 	};
+	static double i_init[] = {0,0,0,0,0,0,0,0,1};
+	static double j_init[] = {0,0,0,1};
+	static double k_init[] = {0,0,0,0,0,0,0,0};
+	static const double learningRate = 1e-2;
 	
-	Matrix *m = mat_new(8,8, input);
+	Matrix *t = mat_new(8,8, input); // targets
+	printf("Input matrix: \n");
+	print_mat(t);
 
-	print_mat(m); 
+	Matrix *xi = mat_new(1,9,i_init);//input layer. 8 units + 1 bias 
+	Matrix *zi = mat_new(1,9,i_init);
+	Matrix *di = mat_new(1,9,i_init);
+	Matrix *xj = mat_new(1,4,j_init);//hidden layer. 3 units + 1 bias 
+	Matrix *zj = mat_new(1,4,j_init);
+	Matrix *dj = mat_new(1,4,j_init);
+	Matrix *xk = mat_new(1,8,k_init);//output layer. no bias units
+	Matrix *zk = mat_new(1,8,k_init);
+	Matrix *dk = mat_new(1,8,k_init);
 
-	randomize(m, 0, 1);	
+	Matrix *W_ij = mat_new(9,4,NULL); // bias "unit" of hidden layer has no incoming weights, so its actually 9x3 not 9x4. I will set col 4 of W_ij to 0 later
+	Matrix *W_jk = mat_new(4,8,NULL);
+	Matrix *dW_ij = mat_new(9,4,NULL); //amount to adjust weights by. same here regarding 9x4 vs 9x3
+	Matrix *dW_jk = mat_new(4,8,NULL);
 
-	print_mat(m);
+	randomize(W_ij, 0.0f, 1.0f);
+	randomize(W_jk, 0.0f, 1.0f);
 
-	print_mat(mat_dot(m,m));
+	//set row 4 of W_ij to 0
+	//to turn off weights from input layer to bias unit of hidden layer
+	size_t i;
+	for (i=0; i<9; ++i) {mat_set(W_ij, i, 4, 0.0f);}
+	
+	size_t nIter, nInput, nUnit;
+	for (nIter=0; nIter<100; ++nIter)
+	{
+		//reconstruct current input
+		for (nInput=0; nInput<8; ++nInput)
+		{
+			//initialize input layer of mlp
+			for (nUnit=0; nUnit<8; ++nUnit)
+			{
+				mat_set(xi, 0, nUnit, mat_get(t, nInput, nUnit));
+				mat_set(zi, 0, nUnit, mat_get(t, nInput, nUnit));
+			}
+			printf("Input layer\n");
+			print_mat(xi);
+			//encode
+			mat_dot(zi, W_ij, xj); //1x9 9x4 1x4
+			mat_apply(xj, sigmoid, zj); //1x4
+			mat_apply(xj, dsigmoid, dj); //1x4
+			printf("Hidden layer\n");
+			print_mat(zj);
+			//decode
+			mat_dot(zj, W_jk, xk); //1x4 4x8 1x8
+			mat_apply(xk, sigmoid, zk); //1x8
+			mat_apply(xk, dsigmoid, dk); //1x8
+			printf("Output layer\n");
+			print_mat(zk);
+
+			//update weights
+			double delta_k_array[8];
+			double t_k, z_k, d_k;
+			for (nUnit=0; nUnit<8; ++nUnit)
+			{
+				t_k = mat_get(t,nInput,nUnit);
+				z_k = mat_get(zk,0,nUnit); 
+				d_k = mat_get(dk,0,nUnit);
+				delta_k_array[nUnit] = -(t_k/z_k - (1-t_k)/(1-z_k)) * d_k / 8; //k-dependent part of dE/dW_jk
+			}
+			Matrix *delta_k = mat_new(1,8,delta_k_array);//1x8
+			Matrix *zj_T = mat_transpose(zj); //4x1
+			mat_dot(zj_T, delta_k, dW_jk); //4x1, 1x8 -> 4x8
+			scalar_mult(dW_jk, -learningRate, dW_jk); //4x8
+			mat_add(W_jk, dW_jk, W_jk); //update W_jk
+			
+			Matrix *delta_j = mat_new(1,4,NULL); //1x4
+			Matrix *W_jk_T = mat_transpose(W_jk); //8x4
+			mat_dot(delta_k, W_jk_T, delta_j); //1x8, 8x4 -> 1x4
+			mat_mult(delta_j, dj, delta_j); //1x4, 1x4 -> 1x4 hadamard product
+			
+			Matrix *zi_T = mat_transpose(zi); //9x1
+			mat_dot(zi_T, delta_j, dW_ij); // 9x1, 1x4 -> 9x4
+			scalar_mult(dW_ij, -learningRate, dW_ij); // 9x4
+			mat_add(W_ij, dW_ij, W_ij); // update W_ij
+
+			//free temporary matrices
+			mat_free(delta_k);
+			mat_free(zj_T);
+			mat_free(delta_j);
+			mat_free(zi_T);
+		}
+	}
 
 	return 0;
 	
--- src/utils.c
+++ src/utils.c
@@ -1,12 +1,27 @@
 #include <utils.h>
 
 
+void mat_apply(Matrix *A, double (*function)(double), Matrix *B)
+{
+	size_t i,j;
+	for (i=0; i<A->nrows; ++i)
+	{
+		double x, z;
+		for (j=0; j<A->ncols; ++j)
+		{
+			x = mat_get(A, i, j);
+			z = function(x);
+			mat_set(B, i, j, z);
+		}
+	}
+}
+
 Matrix* mat_new(const size_t n1, const size_t n2, double input[])
 {
 	Matrix *m = malloc(sizeof(Matrix) + n1*n2*sizeof(double));
 	m->nrows = n1;
 	m->ncols = n2;
-	int i,j;
+	size_t i,j;
 	if (input==NULL) {
 		for (i=0; i<n1; ++i)
 			for (j=0; j<n2; ++j)
@@ -32,24 +47,6 @@ void randomize(Matrix *m, double min, double max)
 	}
 }
 
-Matrix* mat_cpy(const Matrix *A)
-{  
-	size_t i,j,k;
-	Matrix *B;
-	double array_of_a[A->nrows * A->ncols];
-
-	for (i=0; i < A->nrows; ++i){
-		for (j=0; j < A->ncols; ++j){
-			array_of_a[i * A->ncols + j] = mat_get(A,i,j);
-		}
-	}
-
-	B = mat_new(A->nrows, A->ncols, array_of_a);
-	
-	return B;
-}
-
-
 
 double mat_get(const Matrix *m, const size_t i, const size_t j)
 {
@@ -59,14 +56,14 @@ double mat_get(const Matrix *m, const size_t i, const size_t j)
 void mat_set(Matrix *m, const size_t i, const size_t j, double v)
 {
 	m->data[i * m->ncols + j] = v;
+	if (m->data[i * m->ncols + j] != v) {
+		printf("Value not changed\n");
+	}
 }
 
-Matrix* mat_dot(const Matrix *A, const Matrix *B)
+void mat_dot(Matrix *A, Matrix *B, Matrix *C)
 {
 	size_t i,j,k;
-	Matrix *C;
-	C = mat_new(A->nrows, B->ncols,NULL);
-
 	for (i=0; i < A->nrows; ++i){
 		for (j=0; j < B->ncols; ++j){
 			mat_set(C,i,j,0.0f);
@@ -75,55 +72,71 @@ Matrix* mat_dot(const Matrix *A, const Matrix *B)
 			}
 		}
 	}
-	return C;
 }
 
-Matrix* mat_add(const Matrix *A, const Matrix *B)
+void mat_add(Matrix *A, Matrix *B, Matrix *C)
 {	
 	size_t i,j;
-	Matrix *C;
-	C = mat_new(A->nrows, A->ncols, NULL);
-
 	for (i=0; i < A->nrows; ++i){
 		for (j=0; j < B->ncols; ++j){
 			mat_set(C, i, j, mat_get(A,i,j) + mat_get(B,i,j));
 		}
 	}
-	return C;
 }
 
-Matrix* scalar_add(const Matrix *A, const double s)
+//hadamard product
+void mat_mult(Matrix *A, Matrix *B, Matrix *C)
 {	
 	size_t i,j;
-	Matrix *B;
-	B = mat_new(A->nrows, A->ncols, NULL);
+	for (i=0; i < A->nrows; ++i){
+		for (j=0; j < B->ncols; ++j){
+			mat_set(C, i, j, mat_get(A,i,j) * mat_get(B,i,j));
+		}
+	}
+}
 
+Matrix* mat_transpose(Matrix *m)
+{
+	size_t i,j;
+	Matrix *mt = mat_new(m->ncols, m->nrows, NULL);
+	for (i=0; i < m->nrows; ++i){
+		for (j=0; j < m->ncols; ++j){
+			mat_set(mt, j, i, mat_get(m,i,j));
+		}
+	}
+	return mt;
+}
+
+void mat_free(Matrix *m)
+{
+	free(m);
+}
+
+void scalar_add(Matrix *A, const double s, Matrix *B)
+{	
+	size_t i,j;
 	for (i=0; i < A->nrows; ++i){
 		for (j=0; j < A->ncols; ++j){
 			mat_set(B, i, j, mat_get(A,i,j) + s);
 		}
 	}
-	return B;
 }
 
-Matrix* scalar_mult(const Matrix *A, const double s)
+void scalar_mult(const Matrix *A, const double s, Matrix *B)
 {	
 	size_t i,j;
-	Matrix *B;
-	B = mat_new(A->nrows, A->ncols, NULL);
-
 	for (i=0; i < A->nrows; ++i){
 		for (j=0; j < A->ncols; ++j){
 			mat_set(B, i, j, mat_get(A,i,j) * s);
 		}
 	}
-	return B;
 }
 
 
 void print_mat(const Matrix *m)
 {
-	int i,j;
+	size_t i,j;
+	printf("Dimensions: (%lu, %lu)\n", m->nrows, m->ncols);
 	for (i=0; i<m->nrows; ++i)
 	{ 
 		for (j=0; j<m->ncols; ++j)
